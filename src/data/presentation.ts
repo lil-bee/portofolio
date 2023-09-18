c7826d8d7125ad44012a0d4d9290dd92330ae64e
lil-bee <fadhilakbar.2019@student.uny.ac.id>
2023-09-18 19:36:12 +0700

    first commit

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -1,34 +1,31 @@
 type Social = {
-  label: string;
-  link: string;
+	label: string;
+	link: string;
 };
 
 type Presentation = {
-  mail: string;
-  title: string;
-  description: string;
-  socials: Social[];
+	mail: string;
+	title: string;
+	description: string;
+	socials: Social[];
 };
 
 const presentation: Presentation = {
-  mail: "gxanshu@yahoo.com",
-  title: "Hi, I’m Anshu 👋",
-  description:
-    "Howdy, i'm a *indian software programmer* with over *5 years* of web experience. I am currently working with *NextJS and Typescript*. Outside of work I complete my pokemon card collection and learning TypeScript.",
-  socials: [
-    {
-      label: "Twiiter",
-      link: "https://twitter.com/gxanshu",
-    },
-    {
-      label: "Bento",
-      link: "https://bento.me/gxanshu",
-    },
-    {
-      label: "Github",
-      link: "https://github.com/gxanshu",
-    },
-  ],
+	mail: "fdhlakbr280102@gmail.com",
+	title: "Hi, I’m Fadhil 👋",
+	description:
+		"Howdy, i'm a *Indonesian Front-end Developer* and constantly learning new technologies. Currently diving into *React.js* and *Gatsby* to enhance my skills. Always looking for new projects to challenge and improve my abilities. Outside of work I like to watch football match and reading books.",
+	socials: [
+		{
+			label: "Bento",
+			link: "https://bento.me/fdhlakbr",
+		},
+		{
+			label: "Github",
+			link: "https://github.com/lil-bee",
+		},
+	],
 };
 
 export default presentation;
+
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,26 +1,47 @@
 export type Project = {
-  title: string;
-  techs: string[];
-  link: string;
-  isComingSoon?: boolean;
+	title: string;
+	techs: string[];
+	link: string;
+	isComingSoon?: boolean;
 };
 
 const projects: Project[] = [
-  {
-    title: "Arch Linux Package",
-    techs: ["bash"],
-    link: "https://codenanshu.in/projects/ttf-apple-fonts/",
-  },
-  {
-    title: "Firstly",
-    techs: ["React Native", "JavaScript"],
-    link: "https://codenanshu.in/projects/firstly/",
-  },
-  {
-    title: "Astro / Template",
-    techs: ["Astro"],
-    link: "/"
-  },
+	{
+		title: "Quiz App",
+		techs: ["React", "Material UI"],
+		link: "https://quizzical-lilbee.netlify.app/",
+	},
+	{
+		title: "Tenzies Game",
+		techs: ["React", "Material UI"],
+		link: "https://tenzies-lilbee.netlify.app/",
+	},
+	{
+		title: "Meme Generator",
+		techs: ["React", "Material UI"],
+		link: "https://memegenerator-lilbee.netlify.app/",
+	},
+	{
+		title: "Password Generator",
+		techs: ["HTML", "CSS", "Javascript"],
+		link: "https://passwordgenerator-lilbee.netlify.app/",
+	},
+	{
+		title: "Sushiman Landing Page",
+		techs: ["HTML", "CSS", "Responsive"],
+		link: "https://sushiman-lilbee.netlify.app/",
+	},
+	{
+		title: "Edie Homepage",
+		techs: ["HTML", "CSS", "Responsive"],
+		link: "https://ediehomepage-lilbee.netlify.app/",
+	},
+	{
+		title: "Astro - Portofolio",
+		techs: ["Astro"],
+		link: "/",
+	},
 ];
 
 export default projects;
+
