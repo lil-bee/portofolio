d6b8c892603e8508105e68a06cece141eaa00c15
lil-bee <fadhilakbar.2019@student.uny.ac.id>
2024-11-28 16:20:28 +0700

    update: update content

--- src/data/presentation.ts
+++ src/data/presentation.ts
@@ -1,31 +1,34 @@
 type Social = {
-	label: string;
-	link: string;
+  label: string;
+  link: string;
 };
 
 type Presentation = {
-	mail: string;
-	title: string;
-	description: string;
-	socials: Social[];
+  mail: string;
+  title: string;
+  description: string;
+  socials: Social[];
 };
 
 const presentation: Presentation = {
-	mail: "fdhlakbr280102@gmail.com",
-	title: "Hi, I’m Fadhil 👋",
-	description:
-		"Howdy, i'm an *Indonesian Front-end Developer* and constantly learning new technologies. Currently diving into *React.js* and *Gatsby* to enhance my skills. Always looking for new projects to challenge and improve my abilities. Outside of work I like to watch football match and reading books.",
-	socials: [
-		{
-			label: "Bento",
-			link: "https://bento.me/fdhlakbr",
-		},
-		{
-			label: "Github",
-			link: "https://github.com/lil-bee",
-		},
-	],
+  mail: "fdhlakbr280102@gmail.com",
+  title: "Hi, I’m Fadhil 👋",
+  description:
+    "Howdy, i'm an *Indonesian Frontend Engineer* specializing in the *React ecosystem*. Passionate about crafting pretty UI and smooth animations with *Framer Motion*. Currently *exploring backend development* and tackling projects to sharpen my engineering skills. Outside tech, you'll find me watching football or reading book.",
+  socials: [
+    {
+      label: "Bento",
+      link: "https://bento.me/fdhlakbr",
+    },
+    {
+      label: "Linkedin",
+      link: "https://www.linkedin.com/in/fadhilakbar/",
+    },
+    {
+      label: "Github",
+      link: "https://github.com/lil-bee",
+    },
+  ],
 };
 
 export default presentation;
-
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,41 +1,26 @@
 export type Project = {
-	title: string;
-	techs: string[];
-	link: string;
-	isComingSoon?: boolean;
+  title: string;
+  techs: string[];
+  link: string;
+  isComingSoon?: boolean;
 };
 
 const projects: Project[] = [
-	{
-		title: "Quiz App",
-		techs: ["React", "Material UI"],
-		link: "https://quizzical-lilbee.netlify.app/",
-	},
-	{
-		title: "Tenzies Game",
-		techs: ["React", "Material UI"],
-		link: "https://tenzies-lilbee.netlify.app/",
-	},
-	{
-		title: "Quote Generator",
-		techs: ["React", "Typescript", "Chakra UI"],
-		link: "https://quotes-generator-lilbee.netlify.app/",
-	},
-	{
-		title: "Password Generator",
-		techs: ["HTML", "CSS", "Javascript"],
-		link: "https://passwordgenerator-lilbee.netlify.app/",
-	},
-	{
-		title: "Responsive Web Dev - DevChallenges.io",
-		techs: ["HTML", "CSS", "Responsive"],
-		link: "https://github.com/lil-bee/ResponsiveWebDev-DevChallenges.io",
-	},
-	{
-		title: "Astro - Portofolio",
-		techs: ["Astro"],
-		link: "/",
-	},
+  {
+    title: "Devflow - Stackoverflow Clone",
+    techs: ["Next.js", "MongoDB", "Shadcn UI", "Tailwind CSS"],
+    link: "https://devflow-lilbee.vercel.app/",
+  },
+  {
+    title: "AI SEO - Modern & Interactive Sass Landing Page",
+    techs: ["Next.js", "Framer Motion", "Tailwind CSS"],
+    link: "https://aiseo-lilbee.vercel.app/",
+  },
+  {
+    title: "Sushiman - Interactive Restaurant Landing Page",
+    techs: ["HTML", "CSS", "AOS"],
+    link: "https://sushiman-lilbee.netlify.app/",
+  },
 ];
 
 export default projects;
